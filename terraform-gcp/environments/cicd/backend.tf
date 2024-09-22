3ca30f1f415f4a715dfd036b27c27195ca2420ab
praks7v <prakashsatvi29@gmail.com>
2024-09-22 18:15:35 +0530

    modify files and add readme.md

--- terraform-gcp/environments/cicd/backend.tf
+++ terraform-gcp/environments/cicd/backend.tf
@@ -1,12 +1,12 @@
-# terraform {
-#   backend "gcs" {
-#     bucket = "<your-bucket>"
-#     prefix = "terraform/state"
-#   }
-#   required_providers {
-#     google = {
-#       source  = "hashicorp/google"
-#       version = "~> 6.0"
-#     }
-#   }
-# }
+terraform {
+  backend "gcs" {
+    bucket = "tf-state-file-cicd"
+    prefix = "terraform/state"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+  }
+}
